67495df661307a756d6dd8c6a64350780e88982f
Astrashh <Astrashh@users.noreply.github.com>
2021-03-20 14:50:41 +1100

    Search improvements

--- src/test/java/me/astrash/discordwikibot/LuceneTest.java
+++ src/test/java/me/astrash/discordwikibot/LuceneTest.java
@@ -11,10 +11,7 @@ import org.apache.lucene.index.*;
 import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.Query;
-import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.*;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.store.MMapDirectory;
 
@@ -32,60 +29,85 @@ public class LuceneTest {
         String indexDir = "./resources/index";
         String wikiRepoDir = "./resources/wikiRepo";
 
-        // Creating the index
-        MMapDirectory directory = new MMapDirectory(Paths.get(indexDir));
-        Analyzer analyzer = new StandardAnalyzer();
-        IndexWriterConfig config = new IndexWriterConfig(analyzer)
-                .setOpenMode(IndexWriterConfig.OpenMode.CREATE);
-        IndexWriter writer = new IndexWriter(directory, config);
+        // TODO - Add custom analysis functionality
+        Analyzer analyzer = new StandardAnalyzer(); // Used in both indexing and queries
 
-        getFilesWithExtension(wikiRepoDir, ".md").forEach(p ->
-            {
-                try {
-                    indexDoc(writer, Paths.get(p).toAbsolutePath(), FilenameUtils.getBaseName(p));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        );
+        indexWiki(wikiRepoDir, indexDir, analyzer);
 
-        writer.close();
-
-        // Setting up queries
-        IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get(indexDir)));
-        IndexSearcher searcher = new IndexSearcher(reader);
+        // Set up query parser
         String[] fields = new String[] { "title", "contents" };
         HashMap<String, Float> boosts = new HashMap<String, Float>();
         boosts.put("title", (float) 5);
         boosts.put("contents", (float) 1);
         QueryParser parser = new MultiFieldQueryParser(fields, analyzer, boosts);
 
-        // Making a query
-        query("Tree configuration", parser, searcher);
-        query("Config packs", parser, searcher);
-        query("biome config", parser, searcher);
-
-        reader.close();
+        query("Making a pack", indexDir, parser);
+        query("config packs", indexDir, parser);
+        query("community packs", indexDir, parser);
+        query("where can i find packs", indexDir, parser);
+        query("asdkjfasdnf", indexDir, parser);
     }
 
-    private static void query(String input, QueryParser parser, IndexSearcher searcher) throws ParseException, IOException {
+    private static void indexWiki(String wikiRepoDir, String indexDir, Analyzer analyzer) throws IOException, ParseException {
+        long startTime = System.nanoTime();
+        // Creating the index
+        MMapDirectory directory = new MMapDirectory(Paths.get(indexDir));
+        IndexWriterConfig config = new IndexWriterConfig(analyzer)
+                .setOpenMode(IndexWriterConfig.OpenMode.CREATE);
+                // Just creates a new index regardless of if it exists
+        IndexWriter writer = new IndexWriter(directory, config);
+
+        // Index all markdown documents in wiki repo
+
+        // TODO -  Treat subheadings within markdown files as separate documents
+        //         users to search for both full pages AND subheadings within pages.
+
+        for (String p : getFilesWithExtension(wikiRepoDir, ".md")) {
+            indexDoc(writer, Paths.get(p).toAbsolutePath(), FilenameUtils.getBaseName(p));
+        }
+
+        writer.close();
+
+        // Print index speed
+        long duration = (System.nanoTime() - startTime) / 1000000;
+        System.out.println("Index took " + duration + "ms");
+    }
+
+    private static void query(String input, String indexDir, QueryParser parser) throws IOException, ParseException {
+        long startTime = System.nanoTime();
+        IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get(indexDir)));
+        IndexSearcher searcher = new IndexSearcher(reader);
 
         Query query = parser.parse(input);
-        System.out.println("===========================");
+
+        System.out.println("========================================================");
         System.out.println("Searching for: " + input);
         System.out.println("Analyzed query: \"" + query.toString("contents") + "\"");
         int matches = searcher.count(query);
+
+        if (matches < 1) {
+            System.out.println("No matches found");
+            return;
+        }
+
         System.out.println("Found " + matches + " results:");
 
+        // TODO - Delegate search handling outside of method
         TopDocs searchResults = searcher.search(query, matches);
         ScoreDoc[] hits = searchResults.scoreDocs;
 
         for (int i = 0; i < matches; i++) {
             int docNum = hits[i].doc;
             Document doc = searcher.doc(docNum);
-            String title = doc.get("title");
-            System.out.println(title);
+            String title = doc.get("title").replace("-", " ");
+            System.out.println(" - " + title);
         }
+
+        reader.close();
+
+        // Print search speed
+        long duration = (System.nanoTime() - startTime) / 1000000;
+        System.out.println("Query took " + duration + "ms");
     }
 
     private static List<String> getFilesWithExtension(String searchDir, String extension) throws IOException {
